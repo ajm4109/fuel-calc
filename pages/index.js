a2423a8c0d2d9f62ce9a70c69809c9b1a84eaf13
ajm4109 <anthonyj.martin@gmx.com>
2022-03-04 11:52:54 -0700

    fix spelling error on main page fuck...

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const index = () => {
     <div className="flex">
       <Layout />
       <div className="mx-auto flex justify-center p-5">
-        <h1 className="text-center">Welcome to the Water Tech Hepler Site</h1>
+        <h1 className="text-center">Welcome to the Water Tech Helper Site</h1>
       </div>
     </div>
   )
