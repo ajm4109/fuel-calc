2a02a9d02b107a6d3fa6d39bc760cd26db626b28
ajm4109 <anthonyj.martin@gmx.com>
2022-03-04 18:07:17 -0700

    added head>title to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 const index = () => {
   return (
     <div className="block md:flex">
+      <Head>
+        <title>Water Tech</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Layout />
       <div className="mx-auto mt-24 flex justify-center p-5">
         <h1 className="text-center">Welcome to the Water Tech Helper Site</h1>
